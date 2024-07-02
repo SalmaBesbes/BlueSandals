12629c9170c11fc4419631ac1cac268ffd1882ac
Salma Besbes <besbes.salma@gmail.com>
2024-07-02 11:29:15 +0100

    feat: add game and event managers

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+
+
+public class GameManager : EventManager
+{
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        TriggerEvent("InitializeGame");
+    }
+
+}
